bf5a025afd66bafaf706f75510a6ed4c52bb1da5
Humza Aziz <humza@MacBook-Pro-159.local>
2025-03-20 12:05:45 -0600

    syntax error

--- resources/views/admin/admin_dash.blade.php
+++ resources/views/admin/admin_dash.blade.php
@@ -10,9 +10,6 @@
 
 @section('content')
 <div class="container-fluid px-4">
-    <ol class="breadcrumb mb-4">
-        <li class="breadcrumb-item active">Welcome {{ $user_name }}</li>
-    </ol>
 
     <div class="row">
         <!-- Maintenance Issues Card -->
